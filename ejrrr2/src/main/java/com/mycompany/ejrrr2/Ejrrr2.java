b7c9039795e047150b7992877b558fb3db93dc23
leonardo7-del <1590329@senati.pe>
2025-10-21 13:45:13 -0500

    subiendo 2do enunciado

---
+++ ejrrr2/src/main/java/com/mycompany/ejrrr2/Ejrrr2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejrrr2;
+
+/**
+ *
+ * @author vxE
+ */
+public class Ejrrr2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
